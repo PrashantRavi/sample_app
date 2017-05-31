2f2b255bb8d97ceb4a98977e8fc5d9a461883957
travelprashant <ravi.parshant@traveltriangle.com>
2017-05-31 18:41:06 +0530

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  root 'application#hello'
+  root 'static_pages#home'
+  get 'sp/help' => 'static_pages#help'
+  get 'sp/about' => 'static_pages#about'
+  get 'app/hello' => 'application#hello'
 end
